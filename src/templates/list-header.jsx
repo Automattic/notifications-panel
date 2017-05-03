a396a4ffbb4e76cd44801b0afe38cfd560f69968
Dennis Snell <dennis.snell@automattic.com>
2017-05-03 13:09:16 -0700

    Refactor list header component: functional stateless component

--- src/templates/list-header.jsx
+++ src/templates/list-header.jsx
@@ -1,34 +1,11 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
 
-var debug = require('debug')('notifications:list-header');
-
-// from $title-offset in boot/sizes.scss
-var TITLE_OFFSET = 38;
-
-// from $header-height in boot/sizes.scss
-var HEADER_HEIGHT = 34;
-
-const ListHeader = React.createClass({
-    getDefaultProps: function() {
-        return {
-            title: 'None',
-        };
-    },
-
-    render: function() {
-        //		var debugLine = this.props.title + "  offset:" + this.props.offset + " scroll:" + this.props.scroll + " nextOffset: " + this.props.nextOffset + " index:" + this.props.index;
-        var title = this.props.title, classNames = ['wpnc__time-group-title'], style = {};
-        classNames = classNames.join(' ');
-
-        return (
-            <li className="wpnc__time-group-wrap">
-                <div className={classNames} style={style}>
-                    <span className="wpnc__noticon noticon-time" />{title}
-                </div>
-            </li>
-        );
-    },
-});
+export const ListHeader = ({ title = 'None' }) => (
+    <li className="wpnc__time-group-wrap">
+        <div className="wpnc__time-group-title">
+            <span className="wpnc__noticon noticon-time" />{title}
+        </div>
+    </li>
+);
 
 export default ListHeader;
