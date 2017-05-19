9291bf02a4a262bef5a2b19f5dd4b15481208256
Dan Roundhill <dan@automattic.com>
2017-05-19 11:37:34 -0700

    Add a settings button to the first time group header. (#108)
    * First go at adding a settings button to the first time group header.
    
    Refs #104
    
    * Adding a todo for VIEW_SETTINGS
    
    * Fix logic bug in settings placement and refactor time groups
    
    With the original commit in this branch the settings icon was being
    placed on the 'Today' group if it existed, but if no notes were
    available in that group then the icon wasn't added.
    
    This commit changes the behavior so that the settings icon appears on
    the first visible time group header.
    
    Further, some of the time grouping was refactored for simplicity and to
    remove mutation. Most notably the `timeBoundaries` have been adjusted so
    that they form real boundaries (with proper infinite ends) and zipped
    into pairs of boundaries of time values for `(before, after)` referring
    to the range of times within them.
    
    * Simplify ListHeader and move window open to middleware
    
    The `<ListHeader />` didn't need any local state so it has been
    converted into a functional stateless component.
    
    The action to open the settings view has also been turned into a plain
    action and the middleware is picking it up to open a new window.
    
    Future work should intercept that in Calypso to navigate Calypso to the
    settings pane instead of directly opening a new window.
    
    * Introduce custom middleware for integration with Calypso
    
    * Use lodash.findIndex (IE doesn't have it built-in)
    
    * Fix typo: Arrow Left -> Cog
    
    * Bumping package version.

--- src/templates/list-header.jsx
+++ src/templates/list-header.jsx
@@ -2,18 +2,25 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import Gridicon from './gridicons';
+import actions from '../state/actions';
 
-export const ListHeader = ({ title = 'None' }) => (
+export const ListHeader = ({ isFirst, title, viewSettings }) => (
     <li className="wpnc__time-group-wrap">
         <div className="wpnc__time-group-title">
             <Gridicon icon="time" size={18} />{title}
+            {isFirst && <Gridicon icon="cog" size={18} onClick={viewSettings} />}
         </div>
     </li>
 );
 
-export default ListHeader;
+const mapDispatchToProps = {
+    viewSettings: actions.ui.viewSettings,
+};
+
+export default connect(null, mapDispatchToProps)(ListHeader);
