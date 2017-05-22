3972b1c0bcb65df268732f97fc32c0a5f79a90f0
Dennis Snell <dennis.snell@automattic.com>
2017-05-22 11:43:23 -0400

    Modify text rendering inside notes to add styling
    A long-standing issue has been the lack of spacing between paragraphs of
    text. Every line-break has received a `<br />` tag to separate them but
    this left paragraphs flowing without break.
    
    This patch is a hack that adds on some primitive styling in order to get
    "the best bang for the buck" until a proper refactor of the text
    processing occurs.
    
    Namely:
     - Detect lists and make them real lists with `<ul>` and `<li>`
     - Detect emphasized headers like `Next Week: do something`
     - Split paragraphs of text vertically
    
    The new `toBlocks` reducer does the real work here and takes a fairly
    naive approach. Certain bugs exist but I believe that the work in this
    patch will relieve much more frustration than it possibly introduces.
    
    Updates: styling, paragraph detection, todo lists, inline code
    
    Fix: Stop stripping away content after successive `:`s
    
    When trapping on a "header" paragraph we were inserting the content
    _after the header_ with _only the first chunk before the next `:`_
    
    Actually wanted _all of the content after the first `:`_
    
    Update: code fences, styling, todo bullets

--- src/templates/functions.jsx
+++ src/templates/functions.jsx
@@ -2,16 +2,138 @@ import React from 'react';
 
 import { html as toHtml } from '../indices-to-html';
 
+/**
+ * Adds markup to some common text patterns
+ *
+ *  - Bullet lists
+ *  - Todo lists, WP style
+ *  - Inline `code` snippets
+ *  - Code fences
+ *  - Header: description/explanation paragraphs
+ *
+ * Note: This code is only meant to serve until a
+ * proper parser can be built up to convert the
+ * unstructured text into structured data. Since at
+ * this time we still create HTML strings directly
+ * and on every render this function will serve
+ * sufficiently but it should not be looked upon
+ * as good example code!
+ *
+ * @param {String} text input list of blocks as HTML string
+ * @returns {String} marked-up text
+ */
+const toBlocks = text =>
+  text.split('\n').reduce(({ out, inFence, inList }, raw) => {
+    // detect code fences
+    // ```js
+    // doFoo()
+    // ```
+
+    // code fence?
+    // WordPress can replace `` with a fancy double-quote
+    if (/^(```|“`)[a-z1-9]*\s*$/i.test(raw)) {
+      // opening a fence
+      if (!inFence) {
+        return {
+          out: out + '<pre><code>',
+          inFence: true,
+          inList,
+        };
+      }
+
+      // closing a fence
+      return {
+        out: out + '</code></pre>',
+        inFence: false,
+        inList,
+      };
+    }
+
+    // content inside a fence
+    if (inFence) {
+      return {
+        out: out + raw + '\n',
+        inFence,
+        inList,
+      };
+    }
+
+    // emphasized definition-like text
+    // Some value: some description
+    // Header: Value
+    //
+    // Not! This is fun. This: again; isn't emphasized.
+    // May detect false positive if colon found in first sentence.
+    const defined = /^[\w\s-_]+:/.test(raw)
+      ? `<strong>${raw.split(':')[0]}:</strong>${raw.replace(/^[^:]+:/, '')}`
+      : raw;
+
+    // inline `code` snippets
+    const coded = defined.replace(/`([^`]+)`/, '<code>$1</code>');
+
+    // detect list items
+    //  - one
+    //  * two
+    //  [bullet] three
+    //  [dash] four
+    if (/^\s*[*\-\u2012-\u2015\u2022]\s/.test(coded)) {
+      return {
+        out:
+          out +
+            (inList ? '' : '<ul class="wpnc__body-list">') +
+            `<li>${coded.replace(/^\s*[*\-\u2012-\u2015\u2022]\s/, '')}</li>`,
+        inFence,
+        inList: true,
+      };
+    }
+
+    // detect todo lists
+    //  x Done
+    //  o Not done
+    //  O Also not done
+    // X also done
+
+    if (/^\s*x\s/i.test(coded)) {
+      return {
+        out:
+          out +
+            (inList ? '' : '<ul class="wpnc__body-todo">') +
+            `<li class="wpnc__todo-done">${coded.replace(/^\s*x\s/i, '')}</li>`,
+        inFence,
+        inList: true,
+      };
+    }
+
+    if (/^\s*o\s/i.test(coded)) {
+      return {
+        out:
+          out +
+            (inList ? '' : '<ul class="wpnc__body-todo">') +
+            `<li class="wpnc__todo-not-done">${coded.replace(/^\s*o\s/i, '')}</li>`,
+        inFence,
+        inList: true,
+      };
+    }
+
+    // Return a basic paragraph
+    // anything else…
+    return {
+      out: out + (inList ? '</ul>' : '') + `<div>${coded}</div>`,
+      inFence,
+      inList: false,
+    };
+  }, { out: '', inFence: false, inList: false }).out;
+
 export function internalP(html) {
-  return html.split('\n\n').map(function(chunk) {
-    var somewhatRandomKey =
-      'block-text-' + chunk.length + '-' + Date.now() + '-' + Math.random() * 10000;
+  return html.split('\n\n').map((chunk, i) => {
+    const key = `block-text-${i}-${chunk.length}-${chunk.slice(0, 3)}-${chunk.slice(-3)}`;
+    const blocks = toBlocks(chunk);
 
     return (
       <div
-        key={somewhatRandomKey}
+        key={key}
         dangerouslySetInnerHTML={{
-          __html: chunk.replace(/\n/g, '<br/>'),
+          __html: blocks,
         }}
       />
     );
@@ -22,28 +144,23 @@ export function p(html, className) {
   if (undefined === className) {
     className = 'wpnc__paragraph';
   }
-  return html.split('\n\n').map(function(chunk) {
-    var somewhatRandomKey =
-      'block-text-' + chunk.length + '-' + Date.now() + '-' + Math.random() * 10000;
+  return html.split('\n\n').map((chunk, i) => {
+    const key = `block-text-${i}-${chunk.length}-${chunk.slice(0, 3)}-${chunk.slice(-3)}`;
+    const blocks = toBlocks(chunk);
+
     return (
       <div
         className={className}
-        key={somewhatRandomKey}
+        key={key}
         dangerouslySetInnerHTML={{
-          __html: chunk.replace(/\n/g, '<br/>'),
+          __html: blocks,
         }}
       />
     );
   });
 }
 
-export const pSoup = function(items) {
-  return items
-    .map(function(item) {
-      return toHtml(item);
-    })
-    .map(internalP);
-};
+export const pSoup = items => items.map(toHtml).map(internalP);
 
 export function getSignature(blocks, note) {
   if (!blocks || !blocks.length) {
