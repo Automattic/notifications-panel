b024d90b31bce7afba85abc59cb64c741bdc4f84
Dennis Snell <dennis.snell@automattic.com>
2017-05-26 03:08:39 -0400

    Fix: Open links with spans
    Some links have a span surrounding their link text (such as with the
    excerpt) and that prevented us from detecting post links therein.
    
    Now the link intercepter also looks to the event target's parent to see
    if we're one level under a link.

--- src/utils/link-interceptor.js
+++ src/utils/link-interceptor.js
@@ -6,11 +6,12 @@ const openPost = (siteId, postId, href) => ({ type: 'OPEN_POST', siteId, postId,
 export const interceptLinks = event => {
     const { target } = event;
 
-    if ('A' !== target.tagName) {
+    if ('A' !== target.tagName && 'A' !== target.parentNode.tagName) {
         return true;
     }
 
-    const { dataset = {}, href } = target;
+    const node = 'A' === target.tagName ? target : target.parentNode;
+    const { dataset = {}, href } = node;
     const { linkType, postId, siteId } = dataset;
 
     if (!linkType) {
