f72bc268c1fbd0c1a24bb27c725cfed487b44661
Dennis Snell <dennis.snell@automattic.com>
2017-06-22 13:55:32 +0200

    Update/react classes (#165)
    Updates React style to avoid using `React.createClass()`

--- src/templates/actions.jsx
+++ src/templates/actions.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
@@ -13,64 +13,63 @@ import TrashButton from './button-trash';
 
 import { getActions, getReferenceId } from '../helpers/notes';
 
-const ActionsPane = React.createClass({
-  propTypes: {
-    note: React.PropTypes.object.isRequired,
-  },
+const getType = note => (null === getReferenceId(note, 'comment') ? 'post' : 'comment');
 
-  getInitialReplyValue(note) {
-    let ranges, username;
+const getInitialReplyValue = (note, translate) => {
+  let ranges, username;
 
-    if ('user' === note.subject[0].ranges[0].type) {
-      // Build the username from the subject line
-      ranges = note.subject[0].ranges[0].indices;
-      username = note.subject[0].text.substring(ranges[0], ranges[1]);
-    } else if ('user' === note.body[0].type) {
-      username = note.body[0].text;
-    } else {
-      username = null;
-    }
+  if ('user' === note.subject[0].ranges[0].type) {
+    // Build the username from the subject line
+    ranges = note.subject[0].ranges[0].indices;
+    username = note.subject[0].text.substring(ranges[0], ranges[1]);
+  } else if ('user' === note.body[0].type) {
+    username = note.body[0].text;
+  } else {
+    username = null;
+  }
 
-    if (username) {
-      return this.props.translate('Reply to %(username)s...', {
-        args: { username },
-      });
-    }
+  if (username) {
+    return translate('Reply to %(username)s...', {
+      args: { username },
+    });
+  }
 
-    return this.getType(note) === 'post'
-      ? this.props.translate('Reply to post...')
-      : this.props.translate('Reply to comment...');
-  },
+  return getType(note) === 'post'
+    ? translate('Reply to post...')
+    : translate('Reply to comment...');
+};
 
-  getType: note => (null === getReferenceId(note, 'comment') ? 'post' : 'comment'),
+const ActionsPane = ({ global, isApproved, isLiked, note, translate }) => {
+  const actions = getActions(note);
+  const hasAction = types => [].concat(types).some(type => actions.hasOwnProperty(type));
 
-  render() {
-    const { global, note } = this.props,
-      actions = getActions(note),
-      hasAction = types => [].concat(types).some(type => actions.hasOwnProperty(type));
-
-    return (
-      <div className="wpnc__note-actions">
-        <div className="wpnc__note-actions__buttons">
-          {hasAction(['like-post', 'like-comment']) &&
-            <LikeButton {...{ note, isLiked: this.props.isLiked }} />}
-          {hasAction('approve-comment') &&
-            <ApproveButton {...{ note, isApproved: this.props.isApproved }} />}
-          {hasAction('trash-comment') && <TrashButton note={note} />}
-          {hasAction('spam-comment') && <SpamButton note={note} />}
-        </div>
-        {!!actions['replyto-comment'] &&
-          <ReplyInput
-            {...{
-              note,
-              defaultValue: this.getInitialReplyValue(note),
-              global,
-            }}
-          />}
+  return (
+    <div className="wpnc__note-actions">
+      <div className="wpnc__note-actions__buttons">
+        {hasAction(['like-post', 'like-comment']) && <LikeButton {...{ note, isLiked }} />}
+        {hasAction('approve-comment') && <ApproveButton {...{ note, isApproved }} />}
+        {hasAction('trash-comment') && <TrashButton note={note} />}
+        {hasAction('spam-comment') && <SpamButton note={note} />}
       </div>
-    );
-  },
-});
+      {!!actions['replyto-comment'] &&
+        <ReplyInput
+          {...{
+            note,
+            defaultValue: getInitialReplyValue(note, translate),
+            global,
+          }}
+        />}
+    </div>
+  );
+};
+
+ActionsPane.propTypes = {
+  global: PropTypes.object.isRequired,
+  isApproved: PropTypes.bool.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  note: PropTypes.object.isRequired,
+  translate: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state, { note }) => ({
   isApproved: getIsNoteApproved(state, note),
