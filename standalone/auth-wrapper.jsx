608a4c859e3628458e2ed6c4b1a7e5877672fb71
Kirk Wight <kwight@kwight.ca>
2018-01-09 11:52:26 -0800

    Update `w.js` to the latest v57.

--- standalone/auth-wrapper.jsx
+++ standalone/auth-wrapper.jsx
@@ -98,7 +98,7 @@ export const AuthWrapper = Wrapped => class extends Component {
         .then( ( { ID, username } ) => {
             window._tkq = window._tkq || [];
             window._tkq.push( [ 'identifyUser', ID, username ] ) ;
-        } );
+        .catch( () => {} );
 
     render() {
         const { clientId, redirectPath, ...childProps } = this.props;
